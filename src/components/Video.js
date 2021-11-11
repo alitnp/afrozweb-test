02a4ad859fb502abb747116c11ea6bbf7518c931
alitnp <alitnp@ymail.com>
2021-11-11 23:27:51 +0330

    play button added

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,6 +2,7 @@ import "./Video.css";
 import seaSide from "./../asset/images/sea-side.png";
 import seaWave from "./../asset/images/sea-wave2.png";
 import ReadMore from "./global/ReadMore";
+import playIcon from "./../asset/images/part.png";
 
 const Video = () => {
 	return (
@@ -18,6 +19,11 @@ const Video = () => {
 			</div>
 			<div className="player ">
 				<img src={seaWave} alt="sea wave" className="global-elevate-shadow" />
+				<div className="play-button-wrapper">
+					<div className="play-button">
+						<img src={playIcon} alt="" />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
